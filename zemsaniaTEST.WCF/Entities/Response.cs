9da959b00e26f5b2a351b239c1117b8bdc259442
rsojo <49877373+rsojo@users.noreply.github.com>
2021-08-24 09:49:40 -0500

    Add project files.

---
+++ zemsaniaTEST.WCF/Entities/Response.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace zemsaniaTEST.WCF.Entities
+{
+    public class Response<T>
+    {
+        public string Msg { get; set; }
+        public bool Error { get; set; }
+        public T UnitResp { get; set; }
+        List<T> _lst;
+        public List<T> Lst
+        {
+            get { return _lst == null ? new List<T>() : _lst; }
+            set { _lst = value; }
+        }
+    }
+}
